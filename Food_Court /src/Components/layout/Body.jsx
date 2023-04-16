9cd00efef5e4e2516932d90faf7ee1bdfcce420d
Karan Meghe <karanmeghe87@gmail.com>
2023-04-16 15:51:50 +0530

    search bar is accepting value and clearing value after submit

--- Food_Court /src/Components/layout/Body.jsx
+++ Food_Court /src/Components/layout/Body.jsx
@@ -1,8 +1,38 @@
 /* eslint-disable react/react-in-jsx-scope */
+import { useState } from "react";
+// const SearchBar = () => {
+//   return (
+
+//   );
+// };
+
 const Body = () => {
+  const [searchInput, setSearchInput] = useState("");
+
+  const handleForm = (e) => {
+    e.preventDefault();
+    setSearchInput("");
+  };
+
+  const handleInputChange = (e) => {
+    setSearchInput(e.target.value);
+  };
+
   return (
     <main>
-      <h1>Body component</h1>
+      <div className="container-fluid d-flex justify-content-center">
+        <form className="d-flex w-50 m-3" onSubmit={handleForm}>
+          <input
+            className="form-control me-2"
+            type="text"
+            placeholder="Search"
+            aria-label="Search"
+            value={searchInput}
+            onChange={handleInputChange}
+          />
+          <button className="btn btn-outline-secondary">Search</button>
+        </form>
+      </div>
     </main>
   );
 };
---
+++ Food_Court /src/config.jsx
@@ -0,0 +1,3 @@
+// All hardcoded things in config file
+export const IMG_CDN_URL =
+  "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";

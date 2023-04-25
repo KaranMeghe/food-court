0ad70df65a3fc1ab2c3f012eda1973e2a7ec6a27
Karan Meghe <karanmeghe87@gmail.com>
2023-04-26 03:33:04 +0530

    Styling update on Navbar Logo

--- Food_Court /src/App.jsx
+++ Food_Court /src/App.jsx
@@ -2,7 +2,6 @@
 // eslint-disable-next-line no-unused-vars
 import bootstrap from "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./Components/layout/Header";
-import Body from "./Components/layout/Body";
 import { Outlet } from "react-router-dom";
 
 function App() {
--- Food_Court /src/Components/layout/Header.jsx
+++ Food_Court /src/Components/layout/Header.jsx
@@ -16,8 +16,8 @@ const Navbar = () => {
     <header>
       <nav className="navbar navbar-expand-lg navbar-light bg-light d-flex justify-content-between align-items-center px-3 shadow-sm">
         <div className="logo">
-          <Link to="/">
-            <h1 className="navbar-brand">FOOD COURT</h1>
+          <Link to="/" className="text-decoration-none">
+            <h1 className="navbar-brand ">FOOD COURT</h1>
           </Link>
         </div>
 

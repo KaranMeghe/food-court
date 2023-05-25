1581b8ea9d29ce3d00e0dc3aa71172536d9ca988
Karan Meghe <karanmeghe87@gmail.com>
2023-05-25 18:28:22 +0530

    Branch - commiting before switching branch

---
+++ Food_Court /src/Components/Pages/Help.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable react/react-in-jsx-scope */
+
+const About = () => {
+  return (
+    <div className="mt-5">
+      <h1>Section help section </h1>
+    </div>
+  );
+};
+
+export default About;

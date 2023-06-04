4cd3e0e96ba69208d2c1c7bb0306372ca872ccf2
Karan Meghe <karanmeghe87@gmail.com>
2023-06-05 02:54:56 +0530

    Update - image updated

--- Food_Court /src/Components/Pages/Help.jsx
+++ Food_Court /src/Components/Pages/Help.jsx
@@ -1,12 +1,14 @@
+import { useState } from "react";
 import { Link, Outlet } from "react-router-dom";
 import Happy from "../../assets/images/Happy.png";
-import { useState } from "react";
+
 // import Faqs from "../Faqs/Faqs";
 // import PartnerOnboarding from "./PartnerOnboarding";
 // import PartnerOnboarding from "./PartnerOnboarding";
 /* eslint-disable react/jsx-no-undef */
 /* eslint-disable react/react-in-jsx-scope */
 const Help = () => {
+  const [isVisible, setIsVisible] = useState(true);
   return (
     <section id="help" className="bg-light border-bottom">
       <div id="heading_msg_container" className="pt-5">
@@ -14,12 +16,24 @@ const Help = () => {
         <p>Let&apos; s take a step ahead and help you better.</p>
       </div>
 
-      <div className="d-flex justify-content-around ">
+      <div className="d-flex justify-content-around">
+        {isVisible ? <img src={Happy} className="w-50" /> : null}
         <Outlet />
-
         <div className="w-25 d-flex flex-column align-items-center">
-          <Link to="partner-onboarding">PartnerOnboarding</Link>
-          <Link to="faqs">FAQs</Link>
+          <Link
+            to="partner-onboarding"
+            className="text-decoration-none fs-4 my-4"
+            onClick={() => setIsVisible(false)}
+          >
+            Partner Onboarding
+          </Link>
+          <Link
+            to="faqs"
+            className="text-decoration-none fs-4"
+            onClick={() => setIsVisible(false)}
+          >
+            FAQs
+          </Link>
         </div>
       </div>
     </section>

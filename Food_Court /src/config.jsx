dc3fbd620beeeab83a2865bca189b8f6cc18971e
Karan Meghe <karanmeghe87@gmail.com>
2023-08-05 17:53:22 +0530

    E5

--- Food_Court /src/config.jsx
+++ Food_Court /src/config.jsx
@@ -9,14 +9,6 @@ export const FETCH_RESTARUNTS =
 export const FETCH_RESTARUNTS_DETAILS =
   "https://corsproxy.io/?https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=21.098385&lng=79.068965&restaurantId=";
 
-export const PERSON_DETAILS = {
-  info: {
-    name: "Karan meghe",
-    contact: 7066990058,
-    email: "karanmeghe@icloud.com",
-  },
-};
-
 export const FETCH_FAQS =
   "https://corsproxy.io/?https://www.swiggy.com/dapi/support/issues/faq?";
 
